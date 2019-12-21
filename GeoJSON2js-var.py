f4d0b296da20d1bffda88d022853238f52577004
AMgis112 <alex.m.metzler@gmail.com>
2019-12-21 12:14:07 -0600

    Update GeoJSON2js-var.py

--- GeoJSON2js-var.py
+++ GeoJSON2js-var.py
@@ -1,12 +1,12 @@
-// python script to convert geojson to javascript variable
-// https://www.w3schools.com/python/python_file_write.asp
-// var storing the javascript variable (ie var locations = )
-// bring in geojson as string
-// create a javascript file
-// open a javascript file
-// write the js variable
-// write (or append) the geojson string
-// save/close file
+# python script to convert geojson to javascript variable
+# https://www.w3schools.com/python/python_file_write.asp
+# var storing the javascript variable (ie var locations = )
+# bring in geojson as string
+# create a javascript file
+# open a javascript file
+# write the js variable
+# write (or append) the geojson string
+# save/close file
 
 html_str = """
 <table border=1>
